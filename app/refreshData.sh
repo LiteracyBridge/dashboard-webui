27f98c4686a74d98f8af5941ecf810925a458ad0
Bill Evans <billev2k@gmail.com>
2020-10-20 16:41:52 -0700

    Use s3 sync to refresh dashboard data.

--- app/refreshData.sh
+++ app/refreshData.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -x
-# script to refresh dashboard data from dropbox.
+# script to refresh dashboard data from s3.
 
-if [ -z ${dropbox-} ]; then
-  dropbox=~/Dropbox
-fi
-
-rm -rf uf data
-mkdir -p data
-cp  ${dropbox}/DashboardReports/* data/
-for fp in $(tail -n +2 ${dropbox}/DashboardReports/project_list.csv) ; do
-  proj=${fp%,*};
-  echo ${proj};
-  (mkdir -p data/${proj}; cd data/${proj}; pwd; ls; cp -r ${dropbox}/DashboardReports/${proj}/* .)
-done
-
-mkdir -p uf
-cp -r ${dropbox}/AWS-LB/ufReports/uf/* uf/
-
+aws s3 sync --delete s3://dashboard-lb-stats/data data
+aws s3 sync --delete s3://dashboard-lb-stats/uf uf
