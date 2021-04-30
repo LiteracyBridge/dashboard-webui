b597dfa774eb34117d9eaf62b54d96f8026f8f5b
ruby0606 <37945468+ruby0606@users.noreply.github.com>
2021-04-30 15:31:20 -0700

    Update Authentication file to include a more user friendly message for password change (#7)
    * Update Authentication.js
    
    Updating default error message from Amazon for password change to a more user friendly message
    
    * Removed blank space
    
    Removed unintended blank space from 934-948 (15 lines)

--- app/scripts/Authentication.js
+++ app/scripts/Authentication.js
@@ -763,10 +763,14 @@ let Authentication = (function () {
 
             }, function rejected(err) {
                 // There was an error. Show it, but don't close the dialog.
-                alerter.error(err.message || 'Error changing password.');
+                // This is the message that Amazon returns when a user enters a new password with less than 6 characters: "1 validation error detected: Value at 'proposedPassword' failed to satisfy constraint: Member must have length greater than or equal to 6"
+                //Substituting this with a more user-friendly message
+                let msg=err.message || 'Error changing password.'
+                if (err.message && err.message.includes("Member must have length greater than or equal to 6")) {
+                    msg ='New password must have at least 6 characters'
+                }
+                alerter.error(msg);
             });
-
-
         });
 
         fixTabOrdering($dialog);
